3d42d014f1eaa314967c0102e2d8eabc8bb058a4
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-05-19 09:40:43 +0800

    add laravel fortify

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes'
     ];
 
     /**
