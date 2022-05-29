4d022ba0e16598567b0e88e46890b27a3d629358
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-05-29 23:52:25 +0800

    update product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::with('category', 'supplier')->paginate(6);
     }
 
     /**
