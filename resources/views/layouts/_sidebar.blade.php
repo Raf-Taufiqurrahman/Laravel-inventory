13332e38a48bae4d6f1a8ac7eba639181abd2490
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-06-19 18:17:46 +0800

    add permission vehicle

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -149,48 +149,52 @@
                     </li>
                 @endrole
                 @hasanyrole('Admin|Super Admin')
-                    <li class="nav-item">
-                        <a class="nav-link {{ Route::is('admin.dashboard') ? 'active' : '' }}"
-                            href="{{ route('admin.dashboard') }}">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-apps"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <rect x="4" y="4" width="6" height="6" rx="1">
-                                    </rect>
-                                    <rect x="4" y="14" width="6" height="6" rx="1">
-                                    </rect>
-                                    <rect x="14" y="14" width="6" height="6" rx="1">
-                                    </rect>
-                                    <line x1="14" y1="7" x2="20" y2="7"></line>
-                                    <line x1="17" y1="4" x2="17" y2="10"></line>
-                                </svg>
-                            </span>
-                            <span class="nav-link-title">
-                                Dashboard
-                            </span>
-                        </a>
-                    </li>
+                    @can('index-dashboad')
+                        <li class="nav-item">
+                            <a class="nav-link {{ Route::is('admin.dashboard') ? 'active' : '' }}"
+                                href="{{ route('admin.dashboard') }}">
+                                <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-apps"
+                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <rect x="4" y="4" width="6" height="6" rx="1">
+                                        </rect>
+                                        <rect x="4" y="14" width="6" height="6" rx="1">
+                                        </rect>
+                                        <rect x="14" y="14" width="6" height="6" rx="1">
+                                        </rect>
+                                        <line x1="14" y1="7" x2="20" y2="7"></line>
+                                        <line x1="17" y1="4" x2="17" y2="10"></line>
+                                    </svg>
+                                </span>
+                                <span class="nav-link-title">
+                                    Dashboard
+                                </span>
+                            </a>
+                        </li>
+                    @endcan
                     <div class="hr-text hr-text-left ml-2 mb-2 mt-2">Menu</div>
-                    <li class="nav-item">
-                        <a class="nav-link {{ Route::is('admin.category*') ? 'active' : '' }}"
-                            href="{{ route('admin.category.index') }}">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <rect x="8" y="8" width="12" height="12" rx="2">
-                                    </rect>
-                                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
-                                </svg>
-                            </span>
-                            <span class="nav-link-title">
-                                Kategori
-                            </span>
-                        </a>
-                    </li>
+                    @can('index-category')
+                        <li class="nav-item">
+                            <a class="nav-link {{ Route::is('admin.category*') ? 'active' : '' }}"
+                                href="{{ route('admin.category.index') }}">
+                                <span class="nav-link-icon d-md-none d-lg-inline-block mr-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy"
+                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <rect x="8" y="8" width="12" height="12" rx="2">
+                                        </rect>
+                                        <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+                                    </svg>
+                                </span>
+                                <span class="nav-link-title">
+                                    Kategori
+                                </span>
+                            </a>
+                        </li>
+                    @endcan
                     <li class="nav-item">
                         <a class="nav-link {{ Route::is('admin.supplier*') ? 'active' : '' }}"
                             href="{{ route('admin.supplier.index') }}">
